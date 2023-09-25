a3469c333e807f5c94d3fad4edf39fdb08bbbf65
salehloke <58175520+salehloke@users.noreply.github.com>
2023-09-26 07:35:51 +0800

    2 files modified

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Learn <a href="https://nextjs.org">Next.js!</a>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head';
+import Home from './home';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Main() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
 
-      <home></home>
+      <Home />
       <footer className={styles.footer}>
         <a href="https://next.new" target="_blank" rel="noopener noreferrer">
           Created with&nbsp;<b>next.new</b>&nbsp;⚡️
